e3dbfcfe05c9ec95debbfb24e895faadb2d69dc6
Mikhail Fesenko <proggga@gmail.com>
2017-05-12 11:50:31 +0300

    Add get_domains() method to fetcher

--- app/fetcher.py
+++ app/fetcher.py
@@ -1,11 +1,9 @@
-'''
-Module with Database fetcher methods
-'''
+"""Module with Database fetcher methods"""
 import _mysql_exceptions
 
 
 class DataFetcher(object):
-    '''fetcher data from db and return it, also store connectin options'''
+    """fetcher data from db and return it, also store connectin options"""
 
     def __init__(self, connection, is_inner_dns=True):
         self.connection = connection
@@ -14,19 +12,28 @@ class DataFetcher(object):
         self._data = None
 
     def last_fetch_status_ok(self):
-        '''return state of last host request'''
+        """return state of last host request"""
         return self._last_state
 
     def get_data(self):
-        '''return data from last host request'''
+        """return data from last host request"""
         return self._data
 
     def get_hosts(self):
-        '''get active hosts from db with ip addresses'''
-
+        """get active hosts from db with ip addresses"""
         ip_field = 'inIP' if self.is_inner_dns else 'outIP'
         fields = 'name, {} as address'.format(ip_field)
-        query_string = "SELECT {} from hosts where status = 1".format(fields)
+        query = "SELECT {} from hosts where status = 1".format(fields)
+        self._execute(query)
+
+    def get_domains(self):
+        """get active domain by hosts db with"""
+        hostname = 'backup' if self.is_inner_dns else 'moscow'
+        query = 'SELECT url, host, dns_servers as tag from aliases ' \
+                'where dns_servers in ("both", "{}")'.format(hostname)
+        self._execute(query)
+
+    def _execute(self, query_string):
         try:
             self._last_state = True
             self._data = self.connection.query(query_string, [])
---
+++ tests/test_fetcher.py
@@ -0,0 +1,44 @@
+"""Testing Data from db fetcher"""
+import unittest
+from app.fetcher import DataFetcher
+from app.sql import SqlConnection
+from mock import MagicMock
+
+
+class TestDataFetcher(unittest.TestCase):
+    """TestClass for DataFetcher"""
+
+    def setUp(self):
+        self.connection = SqlConnection()
+        self.connection.query = MagicMock(return_value=())
+        self.fetcher = DataFetcher(self.connection)
+
+    def test_get_hosts_by_inner_tag(self):
+        """test get data by inner tag"""
+        self.fetcher.is_inner_dns = True
+        self.fetcher.get_hosts()
+        query = 'SELECT name, inIP as address from hosts where status = 1'
+        self.connection.query.assert_called_with(query, [])
+
+    def test_get_hosts_by_external_tag(self):
+        """test get data by external tag"""
+        self.fetcher.is_inner_dns = False
+        self.fetcher.get_hosts()
+        query = 'SELECT name, outIP as address from hosts where status = 1'
+        self.connection.query.assert_called_with(query, [])
+
+    def test_get_domains_by_inner_tag(self):
+        """test get domains by inner tag"""
+        self.fetcher.is_inner_dns = True
+        self.fetcher.get_domains()
+        query = 'SELECT url, host, dns_servers as tag from aliases ' \
+                'where dns_servers in ("both", "backup")'
+        self.connection.query.assert_called_with(query, [])
+
+    def test_get_domains_by_ext_tag(self):
+        """test get domains by external tag"""
+        self.fetcher.is_inner_dns = False
+        self.fetcher.get_domains()
+        query = 'SELECT url, host, dns_servers as tag from aliases ' \
+                'where dns_servers in ("both", "moscow")'
+        self.connection.query.assert_called_with(query, [])
