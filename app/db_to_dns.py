04887d189f5ec514dd01e591f6706f46240229ce
Mikhail Fesenko <proggga@gmail.com>
2017-05-12 11:12:38 +0300

    refactor: move config to app, renamed

--- app/db_to_dns.py
+++ app/db_to_dns.py
@@ -1,13 +1,15 @@
-'''Main application'''
+"""Main application"""
 from app.fetcher import DataFetcher
 from app.host_updater import HostUpdater
 from app.sql import SqlConnection
+from app.dot_config import DotConfig
 
 
-class App(object):
-    '''Entry point of updater'''
+class DBtoDns(object):
+    """Entry point of updater"""
 
-    def __init__(self, config):
+    def __init__(self, config_path):
+        config = DotConfig.load(config_path)
         self.connection = SqlConnection(**config.connection)
         self.fetcher = DataFetcher(self.connection, config.dns.is_inner)
         self.host_updater = HostUpdater(fetcher=self.fetcher,
@@ -16,11 +18,11 @@ class App(object):
                                         cache_dir=config.zone_cache_dir)
 
     def update_hosts(self):
-        '''main method'''
+        """main method"""
         print("Status is ", self.host_updater.refresh_cache())
         self.host_updater.update()
 
     def update_kodwebs(self):
-        '''main method'''
+        """main method"""
         print("Status is ", self.host_updater.refresh_cache())
         self.host_updater.update()
---
+++ run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""gen new hosts"""
+from app.db_to_dns import DBtoDns
+
+if __name__ == '__main__':
+    DNS_WRITER = DBtoDns('config.yml')
+    DNS_WRITER.update_hosts()
+    DNS_WRITER.update_kodwebs()
