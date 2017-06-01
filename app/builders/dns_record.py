7e36cac777e273f206598843ad3d956a4e29ddaa
Mikhail Fesenko <proggga@gmail.com>
2017-06-01 11:10:05 +0300

    Created fqdn logic, and fix dot problems

--- app/builders/dns_record.py
+++ app/builders/dns_record.py
@@ -5,6 +5,8 @@ class DnsRecord(object):
     """Dns record class"""
 
     def __init__(self, zone, domain_name, references_to):
+        if zone.startswith('.'):
+            zone = zone[1:]
         if domain_name.endswith(zone):
             domain_name = domain_name.replace('.' + zone, '')
         self.domain_name = domain_name
@@ -14,6 +16,21 @@ class DnsRecord(object):
     def add_alias(self):
         """Add alias method"""
         pass
+
+    @property
+    def fqdn(self):
+        """Return fqdn"""
+        domain = self.domain_name
+        fqdn_data = [domain]
+        if isinstance(self.references_to, str):
+            fqdn_data.append(self.zone)
+        else:
+            if domain.endswith(self.references_to.domain_name):
+                fqdn_data.append(self.zone)
+            elif not domain.endswith(self.references_to.fqdn):
+                fqdn_data.append(self.references_to.fqdn)
+
+        return '.'.join(fqdn_data)
 
     def get_references(self):
         """get references object"""
--- tests/builders/test_dns_record.py
+++ tests/builders/test_dns_record.py
@@ -10,19 +10,53 @@ class TestDnsRecordCase(unittest.TestCase):
     def test_constructor(self):
         """test class constructor"""
         record = DnsRecord('com', 'example.com', '1.2.3.4')
+        self.assertTrue(hasattr(record, 'zone'))
         self.assertTrue(hasattr(record, 'domain_name'))
         self.assertTrue(hasattr(record, 'references_to'))
+        self.assertTrue(hasattr(record, 'fqdn'))
         self.assertTrue(hasattr(record, 'add_alias'))
 
-    def test_dns_record_str(self):
+    def test_str_method(self):
         """Test dns record string"""
         record = DnsRecord('com', 'example', '1.2.3.4')
         self.assertEqual(str(record), "example IN A 1.2.3.4")
 
-    def test_dns_record_withzone(self):
+    def test_name_with_zone(self):
         """Test dns record string"""
         record = DnsRecord('com', 'www.example.com', '1.2.3.4')
+        self.assertEqual(record.zone, "com")
+        self.assertEqual(record.domain_name, "www.example")
         self.assertEqual(str(record), "www.example IN A 1.2.3.4")
+
+    def test_fqdn_with_zone(self):
+        """Test dns record string"""
+        record = DnsRecord('com', 'www.example.com', '1.2.3.4')
+        self.assertEqual(record.fqdn, "www.example.com")
+
+    def test_fqdn_without_zone(self):
+        """Test dns record string"""
+        record = DnsRecord('com', 'www.example', '1.2.3.4')
+        self.assertEqual(record.fqdn, "www.example.com")
+
+    def test_fqdn_of_alias(self):
+        """Test dns record string"""
+        record = DnsRecord('com', 'www.example', '1.2.3.4')
+        alias = DnsRecord('com', 'jopka', record)
+        self.assertEqual(alias.fqdn, "jopka.www.example.com")
+
+    def test_fqdn_of_alias_w_fqdn(self):
+        """Test dns record string"""
+        record = DnsRecord('com', 'www.example', '1.2.3.4')
+        alias = DnsRecord('com', 'jopka.www.example.com', record)
+        self.assertEqual(alias.fqdn, "jopka.www.example.com")
+        self.assertEqual(str(alias), "jopka.www.example "
+                         "IN CNAME www.example")
+
+
+    def testzone_with_dot(self):
+        record = DnsRecord('.com', 'www.example.com', '1.2.3.4')
+        self.assertEqual(str(record), "www.example IN A 1.2.3.4")
+        self.assertEqual(record.zone, "com")
 
     def test_dns_alias_str(self):
         """Test dns alias string"""
